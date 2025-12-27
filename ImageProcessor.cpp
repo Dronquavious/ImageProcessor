b1f980734de436ef0c9983a098d94eaa9d370fea
Dronquavious <tambweamani@gmail.com>
2025-12-26 22:59:50 -0600

    fully implemented grayscale and inverting

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <cstring>
 
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -9,15 +10,50 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+void filter_grayscale(unsigned char* image, int width, int height, int channels) {
+	size_t img_size = width * height * channels;
+
+	// channels go in this order [r,g,b,a] *p -> r so to to change g,b or a *(p + index)
+
+	// grayscale = (red * 0.3) + (green * 0.59) + (blue * 0.11)
+
+	for (unsigned char* p = image; p != image + img_size; p += channels) {
+
+		float red = (float)*p;
+		float green = (float)*(p + 1);
+		float blue = (float)*(p + 2);
+
+		float grayValue = (red * 0.3f) + (green * 0.59f) + (blue * 0.11f);
+
+		unsigned char grayscale = (unsigned char)grayValue;
+
+		*p = grayscale;
+		*(p + 1) = grayscale;
+		*(p + 2) = grayscale;
+
+	}
+}
+
+void filter_invert(unsigned char* image, int width, int height, int channels) {
+	size_t img_size = width * height * channels;
+
+	// channels go in this order [r,g,b,a] *p -> r so to to change g,b or a *(p + index)
+	// to invert a color new_color = 255 - old_color
+	for (unsigned char* p = image; p != image + img_size; p += channels) {
+		*p = 255 - *p;
+		*(p + 1) = 255 - *(p + 1);
+		*(p + 2) = 255 - *(p + 2);
+	}
+
+}
+
 int main(int argc, char* argv[])
 {
 
-	if (argc < 3) {
-		std::cout << "Usage: ImageProcessor <input_file> <output_file>" << std::endl;
+	if (argc < 4) {
+		std::cout << "Usage: ImageProcessor <input_file> <output_file> <FLAG>" << std::endl;
 		return -1;
 	}
-
-
 
 	// hold image data
 	int width, height, channels;
@@ -31,35 +67,22 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	std::cout << "Success!" << std::endl;
-	std::cout << "Width: " << width << " Height: " << height << " Channels: " << channels << std::endl;
-
-
-	const int pixel_width = 4;
-
-	size_t img_size = width * height * pixel_width;
-
-	// channels go in this order [r,g,b,a] *p -> r so to to change g,b or a *(p + index)
-
-	// grayscale = (red * 0.3) + (green * 0.59) + (blue * 0.11)
-
-	for (unsigned char* p = image_data; p != image_data + img_size; p += pixel_width) {
-
-		float red =(float) *p;
-		float green = (float) *(p + 1);
-		float blue = (float) *(p + 2);
-
-		float grayValue = (red * 0.3f) + (green * 0.59f) + (blue * 0.11f);
-
-		unsigned char grayscale = (unsigned char)grayValue;
-
-		*p = grayscale;
-		*(p + 1) = grayscale;
-		*(p + 2) = grayscale;
-
+	if (strcmp(argv[3], "--gray") == 0) {
+		filter_grayscale(image_data, width, height, 4);
+	}
+	else if (strcmp(argv[3], "--invert") == 0) {
+		filter_invert(image_data, width, height, 4);
+	}
+	else {
+		std::cout << "Uknown Filter" << std::endl;
+		std::cout << "Available filters: --grayscale, --invert" << std::endl;
+		return -1;
 	}
 
-	stbi_write_png(argv[2], width, height, pixel_width, image_data, width * pixel_width);
+
+	stbi_write_png(argv[2], width, height, 4, image_data, width * 4);
+	std::cout << "IMAGE PROCESSED";
+
 
 	stbi_image_free(image_data);
 
