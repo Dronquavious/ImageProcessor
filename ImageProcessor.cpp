a9b6370011e9f6605237684819b04a6204004c63
Dronquavious <tambweamani@gmail.com>
2025-12-26 22:15:35 -0600

    implemented gray scale via code

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -31,14 +31,27 @@ int main()
 
 	size_t img_size = width * height * pixel_width;
 
+	// channels go in this order [r,g,b,a] *p -> r so to to change g,b or a *(p + index)
+
+	// grayscale = (red * 0.3) + (green * 0.59) + (blue * 0.11)
+
 	for (unsigned char* p = image_data; p != image_data + img_size; p += pixel_width) {
 
-		*(p + 1) = 0;
-		*(p + 2) = 0;
+		float red =(float) *p;
+		float green = (float) *(p + 1);
+		float blue = (float) *(p + 2);
+
+		float grayValue = (red * 0.3f) + (green * 0.59f) + (blue * 0.11f);
+
+		unsigned char grayscale = (unsigned char)grayValue;
+
+		*p = grayscale;
+		*(p + 1) = grayscale;
+		*(p + 2) = grayscale;
+
 	}
 
-	// save the image to a new file called "red_only.jpeg"
-	stbi_write_png("red_only.jpeg", width, height, pixel_width, image_data, width * pixel_width);
+	stbi_write_png("gray-scale.jpeg", width, height, pixel_width, image_data, width * pixel_width);
 
 	stbi_image_free(image_data);
 
