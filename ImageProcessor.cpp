7f79c402d72c012b02c210b35afea1833d781c16
Dronquavious <tambweamani@gmail.com>
2025-12-26 21:15:44 -0600

    Add project files.

---
+++ ImageProcessor.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int main()
+{
+    std::cout << "Image Processor Started..." << std::endl;
+    return 0;
+}
