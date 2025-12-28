e005c77e7da1c26854fb99cc80abb81629f7a996
Dronquavious <tambweamani@gmail.com>
2025-12-27 18:33:57 -0600

    refactor: organize project structure and add documentation
    - Moved source code to `src/` directory
    - Moved third-party libraries to `vendor/` directory
    - Added .gitignore to exclude Visual Studio build artifacts
    - Added better README.md with usage examples and build instructions

---
+++ README.md
@@ -0,0 +1,72 @@
+﻿# 🎨 CLI Image Processor
+
+A high-performance C++ command-line tool for manipulating raw pixel data. Built from scratch to understand memory management, pointer arithmetic, and image convolution algorithms.
+
+## 🚀 Features
+
+* **Zero-Dependency Core:** Uses direct memory manipulation for all image processing logic.
+* **Custom Convolution Engine:** Implements a kernel-based 3x3 box blur algorithm.
+* **Memory Safe:** Robust memory management preventing leaks and "Use-After-Free" errors.
+* **Cross-Format Support:** Reads/Writes JPG, PNG, BMP, and TGA (via `stb_image`).
+
+## 🛠️ How It Works
+
+The tool loads an image into a 1D array of `unsigned char` bytes, manipulates the RGB channels directly via pointer arithmetic, and saves the result.
+
+### Implemented Algorithms:
+1.  **Grayscale:** Uses the Luminance formula `(0.3R + 0.59G + 0.11B)` to match human color perception, rather than a simple average.
+2.  **Invert:** Bitwise negation of color channels `(255 - value)`.
+3.  **Gaussian Blur:** Applies a 3x3 convolution kernel. For stronger effects, the kernel pass is iterated 50x.
+
+## 🛠️ Build Instructions
+
+### Prerequisites
+* **Visual Studio 2022 (or any C++ compiler).
+
+1. Clone the repo
+	```bash
+    git clone [https://github.com/Dronquavious/ImageProcessor.git](https://github.com/Dronquavious/ImageProcessor.git)
+    ```
+2. Open Project: Open ImageProcessor.slnx in Visual Studio.
+3. Build: Press Ctrl + Shift + B or click Build Solution
+
+## 💻 Usage
+
+Run the tool from the command line:
+
+```bash
+./ImageProcessor <input_file> <output_file> <flag>
+```
+
+Examples:
+
+# Convert to Grayscale
+./ImageProcessor input.jpg output.png --grayscale
+
+# Invert Colors (Negative)
+./ImageProcessor input.jpg output.png --invert
+
+# Apply Blur
+./ImageProcessor input.jpg output.png --blur
+
+## 📂 Project Structure
+
+* `src/`: Main application source code
+* `vendor/`: Third-party header-only libraries (stb_image).
+
+## Dependencies
+    // add
+
+
+## 📄 License
+This project is open source and under the MIT License. Feel free to use it for learning or as a base for your own ImageProcessor!
+
+## Contact
+
+If you have questions, feedback, or want to discuss the project, you can reach me at:
+
+- GitHub: @Dronquavious
+- Email: tambweamani@gmail.com
+
+Feel free to reach out — I’m happy to talk about the project or help you get started.
+
--- src/ImageProcessor.cpp
+++ src/ImageProcessor.cpp
@@ -5,10 +5,10 @@
 
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "../vendor/stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "../vendor/stb_image_write.h"
 
 void filter_grayscale(unsigned char* image, int width, int height, int channels) {
 	size_t img_size = width * height * channels;
@@ -47,6 +47,57 @@ void filter_invert(unsigned char* image, int width, int height, int channels) {
 
 }
 
+void filter_blur(unsigned char* image, int width, int height, int channels) {
+	// create a copy of the image so we don't read "changed" pixels
+	unsigned char* copy = new unsigned char[width * height * channels];
+
+	// copy the actual data bytes
+	memcpy(copy, image, width * height * channels);
+
+	// loop through every pixel (skipping the 1-pixel border)
+	// x and y coordinates cause its easier to find neighbors than using pointers
+	for (int y = 1; y < height - 1; y++) {
+		for (int x = 1; x < width - 1; x++) {
+
+			// hold the sum of colors
+			int sum_r = 0;
+			int sum_g = 0;
+			int sum_b = 0;
+
+			// loop through the 3x3 grid around the current pixel (x, y).
+			// The grid goes from (x-1, y-1) to (x+1, y+1).
+
+			for (int ky = -1; ky <= 1; ky++) {
+				for (int kx = -1; kx <= 1; kx++) {
+					int neightbor_index = ((y + ky) * width + (x + kx)) * channels;
+
+					// channels go in this order [r,g,b,a] *p -> r so to to change g,b or a *(p + index)
+
+					sum_r += copy[neightbor_index];
+					sum_g += copy[neightbor_index + 1];
+					sum_b += copy[neightbor_index + 2];
+
+				}
+			}
+
+
+			// average the sums
+			unsigned char avg_r = sum_r / 9;
+			unsigned char avg_g = sum_g / 9;
+			unsigned char avg_b = sum_b / 9;
+
+			// new values to the ORIGINAL image
+			int current_pixel_index = (y * width + x) * channels;
+			image[current_pixel_index] = avg_r;
+			image[current_pixel_index + 1] = avg_g;
+			image[current_pixel_index + 2] = avg_b;
+		}
+	}
+
+	// clean up
+	delete[] copy;
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -73,9 +124,15 @@ int main(int argc, char* argv[])
 	else if (strcmp(argv[3], "--invert") == 0) {
 		filter_invert(image_data, width, height, 4);
 	}
+	else if (strcmp(argv[3], "--blur") == 0) {
+		std::cout << "Blurring 50 times..." << std::endl;
+		for (int i = 0; i < 50; i++) {
+			filter_blur(image_data, width, height, 4);
+		}
+	}
 	else {
 		std::cout << "Uknown Filter" << std::endl;
-		std::cout << "Available filters: --grayscale, --invert" << std::endl;
+		std::cout << "Available filters: --gray, --invert, --blur" << std::endl;
 		return -1;
 	}
 
